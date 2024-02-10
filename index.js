88e2fe0773ef149c12c4fa8aceb828cccb2719ff
Eduardo-Oreia <carloswe3st@gmail.com>
2024-02-10 02:17:40 -0300

    scroll suave

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+// const menuLinks = document.querySelectorAll('nav a[href^="#"]');
+
+// function getDistanceFromTheTop(element) {
+//     const id = element.getAttribute('href');
+//     return document.querySelector(id).offsetTop;
+// }
+
+// function nativeScroll(distanceFromTheTop) {
+//     window.scroll({
+//         top: distanceFromTheTop,
+//         behavior: 'smooth',
+//     });
+// };
+
+// function scrollToSection(event) {
+//     event.preventDefault();
+//     const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
+//     nativeScroll(distanceFromTheTop);
+// }
+
+
+// menuLinks.forEach((link) => {
+//     link.addEventListener('click', scrollToSection);
+// });
+
 document.addEventListener('DOMContentLoaded', () => {
     const navbar = document.querySelector('.header-container');
     const logo = document.querySelector('.logo');
